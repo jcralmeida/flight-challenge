0989b3f086310f7fa73878d5cfdfb7b17d5c8492
iurysalino <iurysalino@gmail.com>
2022-02-01 00:07:04 -0300

    Implemented getStream on Producer

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,11 +1,20 @@
+import java.nio.charset.StandardCharsets;
+
+import javax.xml.ws.EndpointContext;
+
 import org.apache.arrow.flight.FlightClient;
+import org.apache.arrow.flight.FlightDescriptor;
+import org.apache.arrow.flight.FlightInfo;
+import org.apache.arrow.flight.FlightMethod;
+import org.apache.arrow.flight.FlightStream;
 import org.apache.arrow.flight.Location;
 import org.apache.arrow.memory.RootAllocator;
 
-public class Client {
+public class Client{
   public static final RootAllocator allocator = new RootAllocator(Long.MAX_VALUE);
+  public static EndpointContext flightInfo;
 
-  public static void main(String[] args) {
+  public static void main(String[] args){
     final Location location = Location.forGrpcInsecure("localhost", 4152);
 
     FlightClient client = FlightClient.builder()
@@ -14,5 +23,11 @@ public class Client {
         .build();
 
     // Make the call to the getFlightInfo and GetStream
+    FlightDescriptor flight_desc = FlightDescriptor.command("GetFlightInfo".getBytes(StandardCharsets.UTF_8));
+    final FlightInfo flightInfo = client.getInfo(flight_desc);
+    System.out.println("EndPoints: " + flightInfo.getEndpoints() + "Descriptor: "+ flightInfo.getDescriptor());
+    System.out.println("Ticket: " + flightInfo.getEndpoints().get(0).getTicket());
+    final FlightStream flightStream = client.getStream(flightInfo.getEndpoints().get(0).getTicket());
+
   }
 }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,6 +1,14 @@
-import org.apache.arrow.flight.FlightProducer;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.arrow.flight.FlightDescriptor;
+import org.apache.arrow.flight.FlightInfo;
+import org.apache.arrow.flight.FlightMethod;
 import org.apache.arrow.flight.FlightServer;
+import org.apache.arrow.flight.FlightStream;
 import org.apache.arrow.flight.Location;
+import org.apache.arrow.flight.Ticket;
+import org.apache.arrow.flight.perf.impl.PerfOuterClass;
 import org.apache.arrow.memory.RootAllocator;
 
 public class Server {
@@ -10,15 +18,20 @@ public class Server {
     final Location location = Location.forGrpcInsecure("localhost", 4152);
 
     // Instantiate here the Producer you've created.
-    FlightProducer producer;
-
+    ServerFlightProducer serverFlightProducer = new ServerFlightProducer();
 
     //TODO Uncomment this code
-//    final FlightServer server = FlightServer.builder(allocator, location, producer).build().start();
-//    // Print out message for integration test script
-//    System.out.println("Server listening on localhost:" + server.getPort());
-//
-//    server.awaitTermination();
+    final FlightServer server;
+    try {
+      server = FlightServer.builder(allocator, location, serverFlightProducer)
+          .build()
+          .start();
+      // Print out message for integration test script
+      System.out.println("Server listening on localhost:" + server.getPort());
+      server.awaitTermination();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
 }
