d1cd2ff79c921cdc8112d6c50fd68a37411d70c7
iurysalino <iurysalino@gmail.com>
2022-02-02 11:59:39 -0300

    Remove empty lines.

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -7,28 +7,20 @@ import org.apache.arrow.flight.Location;
 import org.apache.arrow.memory.RootAllocator;
 
 class Client {
-  public static final RootAllocator allocator
-      = new RootAllocator(Long.MAX_VALUE);
+  public static final RootAllocator allocator = new RootAllocator(Long.MAX_VALUE);
 
   public static void main(String[] args) {
     final Location location = Location.forGrpcInsecure("localhost", 4152);
-
     FlightClient client = FlightClient.builder()
         .location(location)
         .allocator(allocator)
         .build();
     // Make the call to the getFlightInfo and GetStream
-    final FlightInfo flightInfo = client
-        .getInfo(FlightDescriptor
+    final FlightInfo flightInfo = client.getInfo(FlightDescriptor
             .command("getInfo".getBytes(StandardCharsets.UTF_8)));
-
-    final FlightStream flightStream = client
-        .getStream(flightInfo.getEndpoints().get(0).getTicket());
-
+    final FlightStream flightStream = client.getStream(flightInfo.getEndpoints().get(0).getTicket());
     while (flightStream.next()) {
       System.out.println(flightStream.getRoot().getFieldVectors());
     }
-
   }
-
 }
